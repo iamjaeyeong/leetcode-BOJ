7c02724c29186972c32370db6ca13564bb678d85
Jaeyeong <rlawodud112294@gmail.com>
2022-07-08 16:15:52 +0900

    37 solution

---
+++ LeetCode/37. Sudoku Solver.py
@@ -0,0 +1,58 @@
+class Solution:
+    def solveSudoku(self, board) -> None:
+        zero_pos = []
+        for i in range(9):
+            for j in range(9):
+                if board[i][j]=='.':
+                    board[i][j] = 0
+                    zero_pos.append([j, i])
+                else:
+                    board[i][j] = int(board[i][j])
+        
+        def find_candidates(board, col, row):
+            ret = [1 for i in range(10)]
+            for i in board[row]:
+                ret[i-1] = 0
+            for i in range(9):
+                ret[board[i][col]-1] = 0
+            col = col//3*3
+            row = row//3*3
+            for i in range(3):
+                for j in range(3):
+                    ret[board[row+i][col+j]-1] = 0
+                
+            ret.pop()
+            ret = [i+1 for i in range(9) if ret[i]]
+            return ret
+        
+        def check(board):
+            for i in range(9):
+                for j in range(9):
+                    if board[i][j]==0:
+                        return False
+            return True
+        
+        
+        def dfs(board, cnt, zero_pos):
+            if cnt==len(zero_pos):
+                for i in board:
+                    print(i)
+                return 1
+            col = zero_pos[cnt][0]
+            row = zero_pos[cnt][1]
+
+            candidates = find_candidates(board, col, row)
+            for candidate in candidates:
+                board[row][col] = candidate
+                if dfs(board, cnt+1, zero_pos):
+                    return 1
+            board[row][col] = 0
+        
+        dfs(board, 0, zero_pos)
+        
+        for i in range(9):
+            for j in range(9):
+                board[i][j] = str(board[i][j])
+        
+                    
+        
