537f61ad89788c6617894c1d6e9dc939fc68ab6f
Jaeyeong <rlawodud112294@gmail.com>
2022-07-09 14:47:09 +0900

    38 solution

---
+++ LeetCode/38. Count and Say.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n==1:
+            return "1"
+        base = "11"
+        n -= 1
+        
+        def recursion(n, string):
+            if n==1:
+                return string
+            else:
+                ret = ""
+                prev = string[0]
+                cnt = 1
+                for i in range(1, len(string)):
+                    if string[i]==prev:
+                        cnt += 1
+                    else:
+                        ret += (str(cnt) + prev)
+                        prev = string[i]
+                        cnt = 1
+
+                ret += (str(cnt) + prev)
+                return recursion(n-1, ret)
+        
+        return recursion(n, base)
