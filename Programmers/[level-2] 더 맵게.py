f25194ef3c3946ae25bdf3f4335d715d80572e6b
Jaeyeong <rlawodud112294@gmail.com>
2022-08-01 14:25:05 +0900

    solution

---
+++ Programmers/[level-2] 더 맵게.py
@@ -0,0 +1,30 @@
+import heapq
+
+def solution(scoville, K):
+    answer = 0    
+    h = []
+    for i in scoville:
+        if i<K:
+            heapq.heappush(h, i)
+    is_left = len(h)==len(scoville)
+    
+    if len(h)==0:
+        return 0
+    
+    cnt = 0
+    while 1:
+        if len(h)<2 or h[0]>=K:
+            break
+        else:
+            m1, m2 = heapq[].heappop(h), heapq.heappop(h)
+            heapq.heappush(h, m1 + 2 * m2)
+            cnt += 1
+    
+    if h[0]>=K:
+        return cnt
+    elif is_left:
+        return -1
+    else:
+        return cnt + 1
+    
+    return answer
