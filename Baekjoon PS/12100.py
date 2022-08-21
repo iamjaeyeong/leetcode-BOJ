1663f9b6d95ddb526ba901d7e743c4a32fba96a1
Jaeyeong <rlawodud112294@gmail.com>
2022-08-21 10:53:38 +0900

    solution

--- Baekjoon PS/12100.py
+++ Baekjoon PS/12100.py
@@ -1,80 +1,162 @@
 import sys
 input=sys.stdin.readline
 
-
 n_rows=int(input())
 board=[]
 
 for i in range(n_rows):
     board.append(list(map(int, input().split())))
 
+def merge_left(board, r):
+    prev = board[r][0]
+    prev_idx = 0
+    c = 1
+    while c<len(board):
+        if board[r][c]==0:
+            c += 1
+            continue
+        if board[r][c]==prev:
+            board[r][prev_idx] = prev*2
+            board[r][c] = 0
+            prev = None
+        else:
+            prev = board[r][c]
+            prev_idx = c
+        c += 1
 
-def move(board_, direction, moves):
-    for i in board_:
-        print(i)
-    a, b, c, d=0, 0, 0, 0
-    if direction=='up':
-        cop=[]
-        for i in board_:
-            cop.append(i[:])
-        a=up(cop, moves-1)
-    elif direction=='left':
-        cop=[]
-        for i in board_:
-            cop.append(i[:])
-        b=left(cop, moves-1)
-    elif direction=='right':
-        cop=[]
-        for i in board_:
-            cop.append(i[:])
-        c=right(cop, moves-1)
-    else:
-        cop=[]
-        for i in board_:
-            cop.append(i[:])
-        d=down(cop, moves-1)
-    return max(a, b, c, d)
+def merge_right(board, r):
+    prev = board[r][len(board)-1]
+    prev_idx = len(board)-1
+    c = len(board)-2
+    while c>=0:
+        if board[r][c]==0:
+            
+            c -= 1
+            continue
+        if board[r][c]==prev:
+            board[r][prev_idx] = prev*2
+            board[r][c] = 0
+            prev = None
+        else:
+            prev = board[r][c]
+            prev_idx = c
+        c -= 1
+        
+def merge_up(board, c):
+    prev = board[0][c]
+    prev_idx = 0
+    r = 1
+    while r<len(board):
+        if board[r][c]==0:
+            r += 1
+            continue
+        if board[r][c]==prev:
+            board[prev_idx][c] = prev*2
+            board[r][c] = 0
+            prev = None
+        else:
+            prev = board[r][c]
+            prev_idx = r
+        r += 1
+        
+def merge_down(board, c):
+    prev = board[len(board)-1][c]
+    prev_idx = len(board)-1
+    r = len(board)-2
+    while r>=0:
+        if board[r][c]==0:
+            r -= 1
+            continue
+        if board[r][c]==prev:
+            board[prev_idx][c] = prev*2
+            board[r][c] = 0
+            prev = None
+        else:
+            prev = board[r][c]
+            prev_idx = r
+        r -= 1
+            
+def move_left(board, r):
+    idx = 0
+    for c in range(len(board)):
+        if board[r][idx]:
+            idx += 1
+            continue
+        else:
+            if board[r][c]:
+                board[r][idx] = board[r][c]
+                board[r][c] = 0
+                idx += 1  
+                
+def move_right(board, r):
+    idx = len(board)-1
+    for c in range(len(board)-1, -1, -1):
+        if board[r][idx]:
+            idx -= 1
+            continue
+        else:
+            if board[r][c]:
+                board[r][idx] = board[r][c]
+                board[r][c] = 0
+                idx -= 1 
+                
+def move_up(board, c):
+    idx = 0
+    for r in range(len(board)):
+        if board[idx][c]:
+            idx += 1
+            continue
+        else:
+            if board[r][c]:
+                board[idx][c] = board[r][c]
+                board[r][c] = 0
+                idx += 1  
+                
+def move_down(board, c):
+    idx = len(board)-1
+    for r in range(len(board)-1, -1, -1):
+        if board[idx][c]:
+            idx -= 1
+            continue
+        else:
+            if board[r][c]:
+                board[idx][c] = board[r][c]
+                board[r][c] = 0
+                idx -= 1 
+                
+cases = [board]
+for i in range(5):
+    new_cases = []
+    for case in cases:
+        new_case = [[i for i in j] for j in case]
+        for i in range(len(board)):
+            merge_up(new_case, i)
+            move_up(new_case, i)
+        new_cases.append(new_case)
+        
+        new_case = [[i for i in j] for j in case]
+        for i in range(len(board)):
+            merge_down(new_case, i)
+            move_down(new_case, i)
+        new_cases.append(new_case)
+        
+        new_case = [[i for i in j] for j in case]
+        for i in range(len(board)):
+            merge_right(new_case, i)
+            move_right(new_case, i)
+        new_cases.append(new_case)
+        
+        new_case = [[i for i in j] for j in case]
+        for i in range(len(board)):
+            merge_left(new_case, i)
+            move_left(new_case, i)
+        new_cases.append(new_case)
+    cases = new_cases
 
-def merge_row(row):
-    return
+def return_max(board):
+    return max([max(i) for i in board])
 
-def merge_col(col, c):
-    return
-
-# move 0일 때 빼는걸 move 함수에서 처리하자.
-
-def up(board_, moves):
-    print('up')
-    for c in range(n_rows):
-        merge_col(board_, c)
-    for i in ['up', 'left', 'right', 'down']:
-        return move(board_, i, moves)
-
-def down(board_, moves):
-    print('down')
-    for c in range(n_rows):
-        merge_col(board_,c)
-    for i in ['up', 'left', 'right', 'down']:
-        return move(board_, i, moves)
-
-def right(board_, moves):
-    print('right')
-    for r in range(n_rows):
-        board_[r]=merge_row(board_[r])
-    for i in ['up', 'left', 'right', 'down']:
-        return move(board_, i, moves)
-
-def left(board_, moves):
-    print('left')
-    for r in range(n_rows):
-        board_[r]=merge_row(board_[r])
-    for i in ['up', 'left', 'right', 'down']:
-        return move(board_, i, moves)
-
- 
-for i in ['up', 'left', 'right', 'down']:
-    print(move(board, i, 5))
-
-
-print(move(board, 'left', 1))
-
+answer = 0
+maximums = [return_max(case) for case in cases]
+answer = max(maximums)
+print(answer)
